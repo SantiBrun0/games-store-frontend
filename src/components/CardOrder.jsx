6433740ec44784373cd1039794eb4f602f147c94
Santiago Bruno <santibrunovm@gmail.com>
2023-06-06 14:22:38 -0300

    estilos cardOrder

--- src/components/CardOrder.jsx
+++ src/components/CardOrder.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
 const CardOrder = ({ data }) => {
+  console.log(data);
   return (
     <article className='card-order'>
-        <p>{data.date}</p>
-        <p>{data.fullNameCustomer}</p>
-        <p>{data.dni}</p>
-        <p>{data.phoneNumber}</p>
-        <p>{data.address.street}, {data.address.city}, {data.address.province}, {data.address.country}</p>
+        <p>FECHA: {data.date}</p>
+        <p>TITULAR: {data.fullNameCustomer}</p>
+        <p>DNI: {data.dni}</p>
+        <p>TELEFONO: {data.phoneNumber}</p>
+        <p>DIRECCION: {data.address.street}, {data.address.city}, {data.address.province}, {data.address.country}</p>
+        <p>DETALLE DE ORDEN:</p>
         <ul>
             {
                 data.videogames.map((game, index) => (
@@ -15,6 +17,8 @@ const CardOrder = ({ data }) => {
                 ))
             }
         </ul>
+        <p>MONTO TOTAL: ${data.mount}</p>
+        <p>ESTADO: EN PROGRESO - un operador se comunicará para proceder con el pago y el envío del pedido</p>
     </article>
   )
 }
